a4783ecf06b4bf719d7c8b9aef4d577fa924bf1b
anto24s96 <anto24s96@gmail.com>
2023-12-20 15:52:36 +0100

    exercise completed

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            e_mails: [],
+        };
+    },
+    created() {
+        for(let i=0; i < 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
+                this.e_mails.push(response.data.response);
+            })
+        }
+    },
+}).mount("#app");
